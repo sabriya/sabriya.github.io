7015829d65ee1510de0c5003df3b8c7af39cda30
sabriya <sabriyahome@sabriyas-MacBook-Pro.local>
2014-11-24 20:17:28 -0800

    Portfolio
    First time publishing my portfolio

---
+++ js/myscript.js
@@ -0,0 +1,16 @@
+$(".fancybox").fancybox({
+    afterShow: function() {
+        $(".fancybox-title").wrapInner('<div />').show();
+        
+        $(".fancybox-wrap").hover(function() {
+            $(".fancybox-title").show();
+        }, function() {
+            $(".fancybox-title").hide();
+        });
+    },
+    helpers : {
+        title: {
+            type: 'over'
+        }
+    }
+});
